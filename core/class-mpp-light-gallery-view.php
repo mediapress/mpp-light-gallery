a73240ec5222444b19e93801263ba42d71199f60
raviousprime <ravivats89@gmail.com>
2017-12-05 17:18:06 +0530

    code optimization and documenation view class

--- core/class-mpp-light-gallery-view.php
+++ core/class-mpp-light-gallery-view.php
@@ -1,25 +1,46 @@
 <?php
+/**
+ * Class register new MediaPress photo view
+ *
+ * @package mpp-light-gallery
+ */
+
+
+// If access directly over web code will exit.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
- * View class that actually renders /loads various templates
- *
- * Class MPP_Activity_Light_Gallery_Grid_View
+ * Class MPP_Light_Gallery_View
  */
 class MPP_Light_Gallery_View extends MPP_Gallery_View {
 
+	/**
+	 * Class instance
+	 *
+	 * @var MPP_Light_Gallery_View
+	 */
 	private static $instance = null;
 
+	/**
+	 * MPP_Light_Gallery_View constructor.
+	 */
 	protected function __construct() {
 
 		parent::__construct();
-		//unique id of the view
-		$this->id   = 'light-gallery';
-		$this->name = __( 'Light Gallery Grid', 'mpp-light-gallery' );
-		//supported views
+
+		$this->id                   = 'light-gallery';
+		$this->name                 = __( 'Light Gallery Grid', 'mpp-light-gallery' );
 		$this->supported_views      = array( 'activity', 'gallery' );
 		$this->supported_components = array( 'sitewide', 'groups', 'members' );
 	}
 
+	/**
+	 * Get class instance
+	 *
+	 * @return MPP_Light_Gallery_View
+	 */
 	public static function get_instance() {
 
 		if ( is_null( self::$instance ) ) {
@@ -29,6 +50,12 @@ class MPP_Light_Gallery_View extends MPP_Gallery_View {
 		return self::$instance;
 	}
 
+	/**
+	 * Activity display
+	 *
+	 * @param array $media_ids List of media ids.
+	 * @param int $activity_id Activity id.
+	 */
 	public function activity_display( $media_ids = array(), $activity_id = 0 ) {
 
 		if ( empty( $media_ids ) ) {
@@ -39,9 +66,9 @@ class MPP_Light_Gallery_View extends MPP_Gallery_View {
 	}
 
 	/**
-	 * Single Gallery View
+	 * Single gallery display
 	 *
-	 * @param type $gallery
+	 * @param MPP_Gallery $gallery Gallery object.
 	 */
 	public function display( $gallery ) {
 
@@ -55,8 +82,6 @@ class MPP_Light_Gallery_View extends MPP_Gallery_View {
 			return;
 		}
 
-		//overridden template
 		mpp_get_template( 'buddypress/gallery/views/light-gallery-photo.php', array(), mpp_light_gallery()->get_path() . 'templates' );
 	}
-
 }
